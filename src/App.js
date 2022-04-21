cd9cf724e77117451798544fe288b249b8ab3930
Russell Fraze <russell.fraze@gmail.com>
2022-04-21 13:20:16 -0700

    private route and status hook

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Navbar from './components/Navbar'
+import PrivateRoute from './components/PrivateRoute'
 import Landing from './pages/Landing'
 import ForgotPassword from './pages/ForgotPassword'
 import Profile from './pages/Profile'
@@ -13,7 +14,9 @@ function App() {
         <Routes>
           <Route path='/' element={<Landing />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
-          <Route path='/profile' element={<Profile />} />
+          <Route path='/profile' element={<PrivateRoute />}>
+            <Route path='/profile' element={<Profile />}/>
+          </Route>
           <Route path='/sign-in' element={<SignIn />} />
           <Route path='/sign-up' element={<SignUp />} />
         </Routes>
---
+++ src/components/PrivateRoute.js
@@ -0,0 +1,16 @@
+import { useAuthStatus }  from '../hooks/useAuthStatus'
+import { Navigate, Outlet } from 'react-router-dom'
+
+
+function PrivateRoute() {
+
+    const {loggedIn, checkingStatus} = useAuthStatus() 
+
+    if (checkingStatus) {
+        return <h3>Loading...</h3>
+    }
+
+    return loggedIn ? <Outlet /> : <Navigate to='sign-in' />
+}
+
+export default PrivateRoute
